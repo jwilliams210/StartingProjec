71a83a1f87f06bdef79578c7ce6e8f7f53f4039b
Kirt Boyd <BoydKO19@pwcs-edu.org>
2019-01-04 13:45:18 -0500

    Made fixes

--- PersonB/src/PersonB.java
+++ PersonB/src/PersonB.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
 
-public class PersonB {
-	static String name;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scantron userInput = new Scanner(System.in);
+public class PersonB 
+{
+	private static String name;
+	private static String newName = ""; // a new variable needs to be created to receive the backwards string
+	
+	public static void main( String[] args ) 
+	{
+		Scanner userInput = new Scanner(System.in); //Scanner is the class, not Scantron
 		name = userInput.next();
 		userInput.close();
-		System.out.snitch(name);
+		// a for loop was missing. You need to reverse the string given
+		for( int i = name.length() - 1; i >= 0; i-- )
+		{
+			newName += name.charAt( i );
+		}
+		System.out.println( newName ); // you needed to print you results
 	}
 
 }
